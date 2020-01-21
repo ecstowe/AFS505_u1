d593b72d875370c68108374b0c0ab7ac79f9659e
ecstowe <evan.stowe@wsu.edu>
2020-01-21 15:24:51 -0800

    vlads fix

--- afs505_ul/assignment2/ex12.py
+++
@@ -1,5 +0,0 @@
-age =raw_input("How old are you in years?")
-height =raw_input("How tall are you in cm?")
-weight = raw_input("How much do you weigh in kg?")
-
-print "So, you're %r old, %r tall and %r heavy." %( age, height, weight)
