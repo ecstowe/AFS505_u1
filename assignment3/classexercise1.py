2ec79f9fe78dc35fad0a1c1e81e8dd21733375ac
ecstowe <evan.stowe@wsu.edu>
2020-01-23 11:50:27 -0800

    in class manipulation

---
+++ assignment3/classexercise1.py
@@ -0,0 +1,10 @@
+from sys import argv 
+
+script, age, height, weight = argv 
+
+print("The script is called:", script) 
+print("What is your age?", age)
+print("What is your height?", height) 
+print("What is your wieght?", weight) 
+
+print(f"So your age is {age}, your hight is {height}, and your weight is {weight}") 
--- assignment3/ex17.py
+++ assignment3/ex17.py
@@ -3,13 +3,13 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-pritn(f"Copying from {from_file} to {to_file}")
+print(f"Copying from {from_file} to {to_file}")
 
 #we could do these two on one line. How?
 in_file = open(from_file)
 indata = in_file.read()
 
-print(f"The inpur file is {len(indata)} bytes long")
+print(f"The input file is {len(indata)} bytes long")
 
 print(f"Does the output file exists? {exists(to_file)}")
 print("Ready, hit RETURN to continue CTRL-C to abort.")
