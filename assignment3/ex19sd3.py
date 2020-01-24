4c73005a7349b0dc070dc55fdcbe61771fe0500c
ecstowe <evan.stowe@wsu.edu>
2020-01-23 16:12:18 -0800

    final homework installment of assignment 3

---
+++ assignment3/ex19sd3.py
@@ -0,0 +1,18 @@
+def grumble(pug_count,extra_pugs):
+	print(f"You have {pug_count} pugs in your house!") 
+	print("Damn son! That's almost a grumble!") 
+	print(f"If you had {pug_count} pugs plus {extra_pugs} more pugs, you would have a grumble!")
+
+print("Lets talk about pugs.") 
+grumble(2,1)
+
+print("What if we had more pugs?")
+
+pug_count = 20
+extra_pugs = 100 
+
+grumble(pug_count,extra_pugs)
+
+print("Nah, dawg, that's a pug-apalooza!")
+
+
