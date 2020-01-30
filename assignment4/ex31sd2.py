e3988428565ba826ecc33e2b631904534a06e74d
ecstowe <evan.stowe@wsu.edu>
2020-01-29 22:27:02 -0800

    Assignment 4 work

---
+++ assignment4/ex31sd2.py
@@ -0,0 +1,64 @@
+print("""You have been selected to enter the newly created SPACE FORCE! Membership is mandatory. """) 
+
+print("Would you like a yellow, camoflauge, or pink spacesuit?")
+
+spacesuit = input(">") 
+
+if spacesuit == "yellow": 
+	print("Good choice! You'll blend in with the sun.")
+
+elif spacesuit== "camoflauge": 
+
+	print("Little redneck, but alright.") 
+	
+else: 
+	
+	print("""That's what we're talkin' about. This one features 
+	 1. rocket boosters  or 2. hand grenades, which would you like?""") 
+	
+	pink_upgrade = input(">") 
+	
+	if pink_upgrade == "1": 
+		
+		print("""Oops, looks like money for rocket 
+		fuel has been diverted to build a wall around the earth, you dont get anything cool.""") 
+		
+	else:
+		
+			print("Good call, but use them sparingly. You only have 2.") 
+			
+print("Right then, let's get started. Have you ever experienced zero gravity? Reply with Y or N.") 
+
+gravity = input(">") 
+if gravity == "Y":
+
+	print("What is your moon vertical leap in meters?") 
+	moonjump = input(">")
+	if moonjump > "10": 
+		print("That will do private, we're assigning you to the Interstellar basketball team A.K.A. SPACE JAM!") 
+	elif moonjump == "10": 
+		print(" You can jump the perfect hight to change all of our light bulbs. That is your new job, moon-lightbulb-changer. Sucker.")
+	else:
+		print("You are weak! Don't tell me you have bone spurs too!? Y or N?") 
+		spurs = input(">") 
+		if spurs == "Y": 
+			print("We're sending you to manage a TRUMP tower on Uranus Please step into the spaceship.") 
+		else: 
+			print("Good. You will soon be enrolled in space jumping class. Make sure your space boots fit.") 
+			
+else: 
+	print("Well, there's a first for everything. Step into this here space ship and hold on.") 
+	print("You have been launched into space. Once in orbit, a space cop pulls you over and cites you for not using your rocket-harness.") 
+	print("Would you like to 1. flip the cop the bird or 2. make fun of their MAGA 2030 hat and flip the cop the bird.") 
+	reaction = input(">")
+	if reaction == "1":
+		
+		print("You're under space arrest. You have the right to turn off your spaacesuit intercom now.") 
+	else: 
+		print("The snowflake cop starts crying and fogs up their space-helmet. You make a quick and speedy get away in your newly comandeered spacerocket.") 
+		print("After a couple hours of joy riding your rocket, you sell it to a scrapper on Neptune for a handsome price and spend the rest of your life partying with Neptunians") 
+		
+
+		
+			
+		
